5d0399df83b826249f9d76e42e672c9f8513ee2c
Riccardo C <rusko@palladius.it>
2011-01-20 19:33:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Septober::Application.config.secret_token = '0f742d4b8cce37e47be611f27f6f965c0352737bd15d3e83408f201ebbb01e3a08bd57f8e15e251600d54e534bf0ba34c40f228ce2bfd5bda9ca57e7ce50a965'
