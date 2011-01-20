b9078c80101f386dee171cd44ee69e48c338248b
Riccardo C <rusko@palladius.it>
2011-01-20 19:06:40 +0000

    seeds manuali

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+  ### db.seeds from septober.rb template v3.1.4
+  
+  # Create user obama
+  # Create projects: personal, work, family, love, septober
+  
+  root  = User.create( :name => 'root',  :email => 'root@example.com',  :password => 'Ch4ng3m3!Plz!', :admin => true  )
+  guest = User.create( :name => 'guest', :email => 'guest@example.com', :password => 'guest'        ,  :admin => false )
+  
+  #cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
+  #   Mayor.create(:name => 'Daley', :city => cities.first)
