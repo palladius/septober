2c272317c7205389360d48f557c99aae4cbb8e89
Riccardo Carlesso <rusko@palladius.it>
2011-01-21 08:18:57 +0000

    seeds and description to users

--- db/migrate/20110120193417_add_facebook_id_and_admin_to_users.rb
+++ db/migrate/20110120193417_add_facebook_id_and_admin_to_users.rb
@@ -1,10 +1,12 @@
 class AddFacebookIdAndAdminToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :facebook_id, :string
-    add_column :users, :admin, :boolean,       :default => false
+    add_column :users, :admin,       :boolean,       :default => false
+    add_column :users, :description, :text
   end
 
   def self.down
+    remove_column :users, :description
     remove_column :users, :admin
     remove_column :users, :facebook_id
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,11 @@
   # Create projects: personal, work, family, love, septober
   
   root  = User.create( :name => 'root',  :email => 'root@example.com',  :password => 'Ch4ng3m3!Plz!', :admin => true  )
-  guest = User.create( :name => 'guest', :email => 'guest@example.com', :password => 'guest'        ,  :admin => false )
+  guest = User.create( :name => 'guest', :email => 'guest@example.com', :password => 'guest'        , :admin => false )
   
   #cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
   #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+  # TODO load from fixtures..
+  Fixtures.create_fixtures("db/seeds","projects")
+  
