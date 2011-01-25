10c870f01af8473c8d8ef15b9cfec060d0b395b2
Riccardo C <rusko@palladius.it>
2011-01-25 11:36:08 +0000

    ver1.0.4 bnetter described

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
       # TODO put me in YML..
     :name => 'Septober',
     :headline => 'Yet another ToDo Application, with simplicity in mind',
-    :version => '1.0.2',
+    :version => '1.0.4',
     :copyright => 'Copyright 2011-11 Some rights reserved',
   }
   $APP_NAME = $APP[:name]
