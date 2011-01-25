00d5c0d3bc5675f64bbfdcaa59509195ea82b4ef
Riccardo C <rusko@palladius.it>
2011-01-25 08:35:39 +0000

    refactoerd footer (see CHANGELOG)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
-  $APP_NAME = 'Septober'
-  $APP_HEADLINE = 'Yet another ToDo Application, with simplicity in mind'
+  $APP = {
+      # TODO put me in YML..
+    :name => 'Septober',
+    :headline => 'Yet another ToDo Application, with simplicity in mind',
+    :version => '1.0.2',
+    :copyright => 'Copyright 2011-11 Some rights reserved',
+  }
+  $APP_NAME = $APP[:name]
+  $APP_HEADLINE = $APP[:headline]
   
 end
