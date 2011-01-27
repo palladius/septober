336a2864318783a116483ed7cb2f5ba3b5021bcd
Riccardo C <rusko@palladius.it>
2011-01-27 18:35:35 +0000

    migrated with tags!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110126170354) do
+ActiveRecord::Schema.define(:version => 20110127183341) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,23 @@ ActiveRecord::Schema.define(:version => 20110126170354) do
     t.datetime "updated_at"
     t.boolean  "home_visible", :default => true
     t.boolean  "public",       :default => false
+  end
+
+  create_table "taggings", :force => true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context"
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+
+  create_table "tags", :force => true do |t|
+    t.string "name"
   end
 
   create_table "todos", :force => true do |t|
