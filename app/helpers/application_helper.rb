ffabf1aeefe30cc3503cb3c19e9a98245acd964f
Riccardo C <rusko@palladius.it>
2011-01-30 15:02:32 +0000

    dried VERSION finally!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,14 @@ module ApplicationHelper
   $APP = {
       # TODO put me in YML..
     :name => 'Septober',
-    :headline => 'Yet another ToDo Application, with simplicity kept in mind. Procrastinators unite.. tomorrow! (op. cit.)',
-    :version => '1.3.0',
+    :headline => 'Yet another ToDo Application, with simplicity kept in mind. 
+      Procrastinators unite.. tomorrow! (op. cit.)',
+    :version =>  File.open(RAILS_ROOT + '/VERSION' ).read ,
     :copyright => 'Copyright 2011-11 Some rights reserved',
-    :email => 'palladius@email.it',
+    :email => 'riccardo.carlesso@gmail.com',
   }
   $APP_NAME = $APP[:name]
   $APP_HEADLINE = $APP[:headline]
-  
+  #$APP_VERSION = File.read
+  #File.open(RAILS_ROOT + '/VERSION' ).read
 end
