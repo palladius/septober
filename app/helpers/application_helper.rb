fa495bcd20ef039c7a915f05a71fa532b24c9d18
Riccardo C <rusko@palladius.it>
2011-02-06 13:21:14 +0000

    putting some conf into initializer (faster)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,19 @@
 module ApplicationHelper
-  $APP = {
-      # TODO put me in YML..
-    :name => 'Septober',
-    :headline => 'Yet another ToDo Application, with simplicity kept in mind. 
-      Procrastinators unite.. tomorrow! (op. cit.)',
-    :version     => File.open("#{Rails.root}/VERSION" ).read ,  # RAILS_ROOT
-    :license     => File.open("#{Rails.root}/MIT-LICENSE" ).read ,
-    :copyright   => 'Copyright 2011-11 Some rights reserved',
-    :email       => 'riccardo.carlesso@gmail.com',
-    :base_models => %w{todos projects ric_addons#index mah } ,
-  }
-  $APP[:foo] = :bar
+  # moved to ric_addons.rb
+  
+  #$APP2 = {
+  #    # TODO put me in YML..
+  #  :name        => 'Septober',
+  #  :headline    => 'Yet another ToDo Application, with simplicity kept in mind. 
+  #    Procrastinators unite.. tomorrow! (op. cit.)',
+  #  :version     => File.open("#{Rails.root}/VERSION" ).read ,  # RAILS_ROOT
+  #  :license     => File.open("#{Rails.root}/MIT-LICENSE" ).read ,
+  #  :copyright   => 'Copyright 2011-11 Some rights reserved',
+  #  :email       => 'riccardo.carlesso@gmail.com',
+  #}
+  $APP[:base_models] = %w{todos projects ric_addons#index mah } 
+  #$APP[:foo] = :bar
+  
   $APP_NAME = $APP[:name] + " (obsolete: user $APP instead)"
   $APP_HEADLINE = $APP[:headline]
   #$APP_VERSION = File.read
--- config/initializers/ric_addons.rb
+++ config/initializers/ric_addons.rb
@@ -9,3 +9,15 @@ ActiveRecord::Base.extend Searchable
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # Septober::Application.config.session_store :active_record_store
+
+$APP = {
+  :name        => 'Put your App name here (config/initializers/ric_addons.rb)'
+  #:name        => 'Septober',
+  :headline    => 'Put your App Headline here (config/initializers/ric_addons.rb)',
+  #:headline    => 'Yet another ToDo Application, with simplicity kept in mind.   Procrastinators unite.. tomorrow! (op. cit.)',
+  :version     => File.open("#{Rails.root}/VERSION" ).read ,  # RAILS_ROOT
+  :license     => File.open("#{Rails.root}/LICENSE" ).read ,
+  :copyright   => 'Copyright 2011-11 A few rights reserved (see LICENSE)',
+  # :email       => '',
+  :author      => 'Riccardo Carlesso <riccardo.carlesso@gmail.com>'
+}
