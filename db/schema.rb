31b0272808dea5cfb8e6e6eb871d33c647b3ce8f
Riccardo C <rusko@palladius.it>
2011-02-08 15:50:35 +0000

    migrated todo:photo_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110206160046) do
+ActiveRecord::Schema.define(:version => 20110208152947) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20110206160046) do
     t.integer  "progress_status", :default => 0
     t.boolean  "favorite",        :default => false
     t.text     "sys_notes"
+    t.string   "photo_url"
   end
 
   create_table "users", :force => true do |t|
