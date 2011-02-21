d4021dcb7bf40c4bb2beb060d55336fc658a478d
Riccardo C <rusko@palladius.it>
2011-02-21 17:11:54 +0000

    1.6.10 bnetter icons... trying to fix text but couldnt

--- app/helpers/ric_addons_helper.rb
+++ app/helpers/ric_addons_helper.rb
@@ -21,18 +21,20 @@ module RicAddonsHelper
   # "aaa.png" =>  adds
   def photo_real_url(model, opts={})
     image_dir = opts.fetch :image_dir, '/photos/' # contacts
-    my_photo_url = model.photo_url rescue '' # default_model.png
-    my_photo_url ||= "default.png"
+    my_photo_url = model.photo_url.to_s  # send( opts.fetch(:image_method, :photo_url)) # rescue '' # default_model.png
+    my_photo_url = "default.png" if my_photo_url == '' #rescue nil # ||= "default.png"    
     #my_photo_url = $anonymnous_default_path if ( my_photo_url.to_s  == '' )
     my_photo_url =  image_dir + my_photo_url unless my_photo_url.match(/^http:\/\/|^\//) # i.e. http://www.faceboo.com/myphoto.jpg
     my_photo_url = '/images'    + my_photo_url if my_photo_url.match(/^\//)                      # i.e. /icons/... is local
     my_photo_url 
   end
+
   
   def render_photo_url(model, opts={})
     model_type = model.class.to_s
     height = opts.fetch( :height, 100 )
-    image_tag(photo_real_url(model), :height => height , :alt => "Photo for #{model_type}: '#{model}'")
+    url = photo_real_url(model,opts)
+    image_tag(url, :height => height , :alt => "Photo for #{model_type}: '#{model}' URL='#{url}'")
   end
   
 end
