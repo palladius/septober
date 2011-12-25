14de46a1d85c9816c94a45c600e56642d4bd3835
Riccardo Carlesso <riccardo_carlesso@dell.com>
2011-12-25 16:31:49 +0100

    Secured the logged out page for tags/taggings

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,12 +4,13 @@ class TagsController < ApplicationController
 #  1.  copialo da ric_addressbook
 #  2.  e a posto RSS  
   def show
-    @tag = Tag.find(params[:id]) 
-    @taggings =  Tagging.find_all_by_tag_id(params[:id])
+    #return unless current_user
+    @tag      = current_user ? Tag.find(params[:id]) : ""
+    @taggings = current_user ? Tagging.find_all_by_tag_id(params[:id]) : []
   end
   
   def index
-    @tags = Tag.all
+    @tags = current_user ? Tag.all : []
   end
   
 end
