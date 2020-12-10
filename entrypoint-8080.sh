c74e227c74143c4d029bf55a12fe79f7f8862206
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-12-10 23:58:14 +0100

    entrypoint-added

---
+++ entrypoint-8080.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# questo supera ed estende il RUN IN PROD basandosi sulla sola regola di RAILS_ENV :) 
+# prepare
+# ActionView::Template::Error (The asset "home.png" is not present in the asset pipeline.
+. .env
+
+VER="$(cat VERSION)"
+MYPORT="${PORT:-8080}"
+APP_NAME="${APPNAME:-entrypoint-sobenme}"
+
+echo "[$APP_NAME-v$VER-entrypoint] BEGIN. Args: $*"
+    
+if printenv RAILS_ENV | grep -q production ; then
+    echo "[$APP_NAME-v$VER-entrypoint] Riccardo I believe this is PROD Lets scagnozz the dogs on port $MYPORT"
+    export RAILS_LOG_TO_STDOUT=yesplease 
+    export RAILS_ENV=production
+    export RACK_ENV production
+
+    source .env
+    source .env.production
+
+    bundle exec rake assets:precompile
+
+    #bundle exec rake db:migrate
+
+    bundle exec rails s -b 0.0.0.0 -p $MYPORT
+else
+    echo "[$APP_NAME-v$VER-entrypoint] Riccardo I believe this is DEV Lets keep it easy peasy on port $MYPORT"
+    source .env
+    bundle exec rake assets:precompile
+    bundle exec rails s -b 0.0.0.0 -p $MYPORT
+fi
+
+echo "[$APP_NAME-v$VER-entrypoint] END. Calling now Args in case you gave me: $*"
+"$@"
