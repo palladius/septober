0d9f82dad3f6d9986d31e18a0aedbc5ef6e5364b
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-12-24 18:20:45 +0100

    cloning septober dev part...

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -90,7 +90,7 @@ class TodosController < ApplicationController
   end
   
   def destroy
-    #raise "Destroy exception: (i thought id never get here!)"
+    raise "Destroy exception: (i thought id never get here!)"
     #flash[:notice] = "Starting to destroy todo ##{@todo.id}..."
     @todo = Todo.find(params[:id])
     @todo.destroy 
