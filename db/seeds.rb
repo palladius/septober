3532885c7d01319fe241a36cb7b1c1732a5a1740
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-12-28 21:06:48 +0100

    rake seed works. docker-compose WORKS!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
   ### db.seeds from septober.rb template v3.1.4_159c
   
   # Create projects: personal, work, family, love, septober
+
+puts "[rake db:seed] START. Provisioning some users.."
+
+admin_password = ENV.fetch("ADMIN_PASSWORD",  false) # 'seed-Ch4ng3m3!Plz!'
+if admin_password
+  puts(green "Provisioning root user with password [#{admin_password}]. Write it down as it wont be easy to guess later on! #yoro")
+  root  = User.create( :username => 'root',  :email => 'root@example.com',  :password => admin_password, :admin => true  ) # .save!
+else
+  puts(red "Skipping root user since no ADMIN_PASSWORD was provided from ENV.. #giargiana")
+end
+
+guest = User.create( :username => 'guest', :email => 'guest@example.com', :password => 'seed-guest' , :admin => false ) # .save!
   
-  root  = User.create( :name => 'root',  :email => 'root@example.com',  :password => 'seed-Ch4ng3m3!Plz!', :admin => true  )
-  guest = User.create( :name => 'guest', :email => 'guest@example.com', :password => 'seed-guest'        , :admin => false )
-    
   # Adding TODOs
   # Please check Todo.provision_for_user class method which adds a lot of these once a user is created.
+puts "[rake db:seed] END. User.all: #{User.all.map{|x| x.to_s }}"
