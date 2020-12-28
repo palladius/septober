d205341105bcb8a1c18bf688748fabdce2ceb0d6
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-12-28 21:25:24 +0100

    minor

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,17 @@ puts "[rake db:seed] START. Provisioning some users.."
 admin_password = ENV.fetch("ADMIN_PASSWORD",  false) # 'seed-Ch4ng3m3!Plz!'
 if admin_password
   puts(green "Provisioning root user with password [#{admin_password}]. Write it down as it wont be easy to guess later on! #yoro")
-  root  = User.create( :username => 'root',  :email => 'root@example.com',  :password => admin_password, :admin => true  ).save!
+  root  = User.create( :username => 'root',  :email => 'root@example.com',  :password => admin_password, :admin => true  ) #.save!
 else
   puts(red "Skipping root user since no ADMIN_PASSWORD was provided from ENV.. #giargiana")
 end
 
-guest = User.create( :username => 'guest', :email => 'guest@example.com', :password => 'seed-guest' , :admin => false ).save!
+guest = User.create( :username => 'guest', :email => 'guest@example.com', :password => 'seed-guest' , :admin => false )#.save!
   
+# If I leave the /save! It will exit with error and wont execute rails at all in docker-compose -> lets make it more robust here.
+
   # Adding TODOs
   # Please check Todo.provision_for_user class method which adds a lot of these once a user is created.
 puts "[rake db:seed] END. User.all: #{User.all.map{|x| x.to_s }}"
+
+exit 0
